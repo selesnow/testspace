ec89955f7fdb93803c02d05c2494cc504c334b01
Alexey Seleznev <selesnow@mail.ru>
2018-11-21 13:30:20 +0200

    Update myTarGetAdList.R

--- rmytarget2/R/myTarGetAdList.R
+++ rmytarget2/R/myTarGetAdList.R
@@ -10,13 +10,13 @@ myTarGetAdList <-
     count  <- NULL
     result <- list()
     
-    packageStartupMessage("Loading |",appendLF = T)
+    packageStartupMessage("Loading |",appendLF = F)
     
     while ( is.null(count) || count > offset ) {
       ads <- GET(stringr::str_interp("${getOption('rmytarget.url')}api/v2/banners.json?fields=id,status,urls,campaign_id,textblocks,moderation_status,created,updated,call_to_action&limit=${limit}&offset=${offset}"),add_headers(Authorization = paste0("Bearer ",auth$access_token)))
       stop_for_status(ads)
       adsRaw <- content(ads, "parsed", "application/json")
-    
+      
       for (i in 1:length(adsRaw$items)) {
         temp <- list(id                = adsRaw$items[[i]]$id,
                      status            = adsRaw$items[[i]]$status,
@@ -31,19 +31,20 @@ myTarGetAdList <-
                      url_object_id     = adsRaw$items[[i]]$urls$primary$url_object_id,
                      url_object_type   = adsRaw$items[[i]]$urls$primary$url_object_type,
                      url_id            = adsRaw$items[[i]]$urls$primary$id)
-       
-         result <- append(result, list(temp))
-         rm(temp)
-         packageStartupMessage("=",appendLF = F)
-         Sys.sleep(0.1)
+        
+        result <- append(result, list(temp))
+        rm(temp)
+        
       }
       
+      packageStartupMessage("=",appendLF = F)
+      Sys.sleep(0.7)
       count  <- adsRaw$count
       offset <- offset + limit
       
     }
+    
     packageStartupMessage("| Done",appendLF = T)
     adsList <- data.frame(do.call("rbind", result))
     return(adsList)
-}
-
+  }
